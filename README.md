ec1fc492a180bf0213c92c86ddb780634d943c76
Michael Murray <mmurray.wa@gmail.com>
2010-11-23 17:07:30 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# play-jqvalidate
+
+Client-side form validation based on your Play model validation annotations.
+
+## Demo
+
+[http://play-jqvalidate.appspot.com/](http://play-jqvalidate.appspot.com/) is an example of a form validated using this module.
+
+## Dependencies
+
+The `play-jqvalidate` JavaScript components depend on the [jQuery](http://jquery.com/) library.
+
+## Installation
+
+### Install the module
+
+Install the jqvalidate module from the modules repository:
+
+    play install jqvalidate
+
+### Enable the module
+
+After installing the module, add the following to your `application.conf` to enable it:
+
+    module.jqvalidate=${play.path}/modules/jqvalidate
+
+## Usage
+
+### Use the `jqvalid.form` tag in your view
+
+The `jqvalid.form` tag is identical to the built-in Play form tag except it outputs some JavaScript that prepares the form to be validated by the jQuery validation plugin.
+
+### Use the `jqvalid.field` tag in your view
+
+The `jqvalidate.field` tag is identical to the built-in Play field tag except it provides an extra property on the field, `field.validationData`. You need to put this data in an HTML5 data attribute called `data-validation` on your `input`, `select`, or `textarea` element.  
+
+    #{jqvalid.field 'task.details'}
+		<p>
+	  		<label>&{field.name}</label>
+	  		<input type="text" data-validate="${field.validationData}" id="${field.id}" name="${field.name}" value="${field.value}" class="${field.errorClass}">
+	  		<span class="error">${field.error}</span>
+		</p>
+	#{/}
+	
+## Supported Annotations
+
+The module currently supports the following annotations:
+
+	* `Required`
+	* `Email`
+	* `Min`
+	* `Max`
+	* `Range`
+	* `MinSize`
+	* `MaxSize`
+	* `URL`
+
+## Example
+
+### Model
+
+	@Entity
+	public class Task extends Model {
+	    @Required @Range(max=10,min=1) public int priority;
+	    @Required @Email public String authorEmail;
+	    @Required @URL public String authorUrl;
+	    @Required public String details;
+	}
+
+### View
+	
+	#{jqvalid.form @Tasks.save()}
+	  	#{jqvalid.field 'task.details'}
+			<p>
+			  <label>&{field.name}</label>
+			  <input type="text" data-validate="${field.validationData}" id="${field.id}" name="${field.name}" value="${field.value}" class="${field.errorClass}">
+			  <span class="error">${field.error}</span>
+			</p>
+		#{/}
+		#{jqvalid.field 'task.priority'}
+			<p>
+			  <label>&{field.name}</label>
+			  <input type="text" data-validate="${field.validationData}" id="${field.id}" name="${field.name}" value="${field.value}" class="${field.errorClass}">
+			  <span class="error">${field.error}</span>
+			</p>
+		#{/}
+		#{jqvalid.field 'task.authorEmail'}
+			<p>
+			  <label>&{field.name}</label>
+			  <input type="text" data-validate="${field.validationData}" id="${field.id}" name="${field.name}" value="${field.value}" class="${field.errorClass}">
+			  <span class="error">${field.error}</span>
+			</p>
+		#{/}
+		#{jqvalid.field 'task.authorUrl'}
+			<p>
+			  <label>&{field.name}</label>
+			  <input type="text" data-validate="${field.validationData}" id="${field.id}" name="${field.name}" value="${field.value}" class="${field.errorClass}">
+			  <span class="error">${field.error}</span>
+			</p>
+		#{/}
+		<p>
+			<input type="submit" value='Create Task'>
+		</p>
+	#{/jqvalid.form}
+
+### Result
+
+You can view the example form live at [http://play-jqvalidate.appspot.com/](http://play-jqvalidate.appspot.com/)
+
+
+
+	
---
+++ app/tags/jqvalidate/JqValidateTags.java
@@ -0,0 +1,219 @@
+package tags.jqvalidate;
+
+import groovy.lang.Closure;
+
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+
+import play.Logger;
+import play.data.validation.*;
+import play.i18n.Messages;
+import play.libs.I18N;
+import play.mvc.Router.ActionDefinition;
+import play.mvc.Scope.Flash;
+import play.templates.FastTags;
+import play.templates.JavaExtensions;
+import play.templates.GroovyTemplate.ExecutableTemplate;
+
+@FastTags.Namespace("jqvalid")
+public class JqValidateTags extends FastTags {
+    
+    /**
+     * Generates a html form element linked to a controller action, backed by client-side validation
+     * @param args tag attributes
+     * @param body tag inner body
+     * @param out the output writer
+     * @param template enclosing template
+     * @param fromLine template line number where the tag is defined
+     */
+    public static void _form(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
+        ActionDefinition actionDef = (ActionDefinition) args.get("arg");
+        if (actionDef == null) {
+            actionDef = (ActionDefinition) args.get("action");
+        }
+        String enctype = (String) args.get("enctype");
+        if (enctype == null) {
+            enctype = "application/x-www-form-urlencoded";
+        }
+        if (actionDef.star) {
+            actionDef.method = "POST"; // prefer POST for form ....
+        }
+        if (args.containsKey("method")) {
+            actionDef.method = args.get("method").toString();
+        }
+        if (!("GET".equals(actionDef.method) || "POST".equals(actionDef.method))) {
+            String separator = actionDef.url.indexOf('?') != -1 ? "&" : "?";
+            actionDef.url += separator + "x-http-method-override=" + actionDef.method.toUpperCase();
+            actionDef.method = "POST";
+        }
+        String id = args.containsKey("id") ? (String)args.get("id") : "play-jqvalid-form__"+(Math.random()*10000); 
+        out.println("<form id=\""+ id +"\" action=\"" + actionDef.url + "\" method=\"" + actionDef.method.toUpperCase() + "\" accept-charset=\"utf-8\" enctype=\"" + enctype + "\" " + serialize(args, "action", "method", "accept-charset", "enctype") + ">");
+        if (!("GET".equals(actionDef.method))) {
+            _authenticityToken(args, body, out, template, fromLine);
+        }
+        out.println(JavaExtensions.toString(body));
+        out.println("</form>");
+        out.print("<script type='text/javascript'> $(document).ready(function(){ ");
+        out.print("$.metadata.setType('html5'); ");
+        out.print("$('#"+id+"').validate({ meta:'validate' }); ");
+        out.print("}); </script>");
+    }
+    
+    private static String buildValidationDataString(Field f){
+        StringBuilder result = new StringBuilder("{");
+        List<String> rules = new ArrayList<String>();
+        Map<String,String> messages = new HashMap<String,String>();
+        try{
+        Required required = f.getAnnotation(Required.class);
+        if(required != null){
+            rules.add("required:true");
+            if(required.message() != null){
+                messages.put("required", Messages.get(required.message()));
+            }
+        }
+        Min min = f.getAnnotation(Min.class);
+        if(min != null){
+            rules.add("min:"+new Double(min.value()).toString());
+            if(min.message() != null){
+                messages.put("min", Messages.get(min.message(), null, min.value()));
+            }
+        }
+        Max max = f.getAnnotation(Max.class);
+        if(max != null){
+            rules.add("max:"+new Double(max.value()).toString());
+            if(max.message() != null){
+                messages.put("max", Messages.get(max.message(), null, max.value()));
+            }
+        }
+        Range range = f.getAnnotation(Range.class);
+        if(range != null){
+            rules.add("range:["+new Double(range.min()).toString()+", "+new Double(range.max()).toString()+"]");
+            if(range.message() != null){
+                messages.put("range", Messages.get(range.message(), null, min.value(), max.value()));
+            }
+        }
+        MaxSize maxSize = f.getAnnotation(MaxSize.class);
+        String t = maxSize == null ? "null" : "not null";
+        if(maxSize != null){
+            rules.add("maxlength:"+new Integer(maxSize.value()).toString());
+            if(maxSize.message() != null){
+                messages.put("maxlength", Messages.get(maxSize.message(), null, maxSize.value()));
+            }
+        }
+        MinSize minSize = f.getAnnotation(MinSize.class);
+        if(minSize != null){
+            rules.add("minlength:"+new Integer(minSize.value()).toString());
+            if(minSize.message() != null){
+                messages.put("minlength", Messages.get(minSize.message(), null, minSize.value()));
+            }
+        }
+        URL url = f.getAnnotation(URL.class);
+        if(url != null){
+            rules.add("url:true");
+            if(url.message() != null){
+                messages.put("url", Messages.get(url.message()));
+            }
+        }
+        Email email = f.getAnnotation(Email.class);
+        if(email != null){
+            rules.add("email:true");
+            if(email.message() != null){
+                messages.put("email", Messages.get(email.message()));
+            }
+        }
+        }catch(Exception e){
+            Logger.info(e.getMessage());
+        }
+        if(rules.size() > 0){
+            boolean first = true;
+            for(String rule : rules){
+                if(first){
+                    first = false;
+                }else{
+                    result.append(",");
+                }
+                result.append(rule);
+            }
+        }
+        if(messages.size() > 0){
+            result.append(",messages:{");
+            boolean first = true;
+            for(String key : messages.keySet()){
+                if(first){
+                    first = false;
+                }else{
+                    result.append(",");
+                }
+                result.append("\"");
+                result.append(key);
+                result.append("\"");
+                result.append(":");
+                result.append("\"");
+                try{
+                    result.append(messages.get(key));
+                }catch(Exception e){
+                }
+                Logger.info("hi6");
+                result.append("\"");
+                Logger.info("hi");
+            }
+            Logger.info("hi");
+            result.append("}");
+        }
+        Logger.info(result.toString());
+        result.append("}");
+        return result.toString();
+    }
+    
+    public static void _field(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
+        Map<String,Object> field = new HashMap<String,Object>();
+        String _arg = args.get("arg").toString();
+        field.put("name", _arg);
+        field.put("id", _arg.replace('.','_'));
+        field.put("flash", Flash.current().get(_arg));
+        field.put("flashArray", field.get("flash") != null && !field.get("flash").toString().isEmpty() ? field.get("flash").toString().split(",") : new String[0]);
+        field.put("error", Validation.error(_arg));
+        field.put("errorClass", field.get("error") != null ? "hasError" : "");
+        
+        
+        
+//        field.put("validationData", );
+        String[] pieces = _arg.split("\\.");
+        Object obj = body.getProperty(pieces[0]);
+        if(obj != null){
+            if(pieces.length > 1){
+                for(int i = 1; i < pieces.length; i++){
+                    try{
+                        Field f = obj.getClass().getField(pieces[i]);
+                        if(i == (pieces.length-1)){
+                            field.put("validationData", buildValidationDataString(f));
+                            
+                            try{
+                                Method getter = obj.getClass().getMethod("get"+JavaExtensions.capFirst(f.getName()));
+                                field.put("value", getter.invoke(obj, new Object[0]));
+                            }catch(NoSuchMethodException e){
+                                field.put("value",f.get(obj).toString());
+                            }
+                        }else{
+                            obj = f.get(obj);
+                        }
+                    }catch(Exception e){
+                        // if there is a problem reading the field we dont set any value
+                    }
+                }
+            }else{
+                field.put("value", obj);
+            }
+        }
+        body.setProperty("field", field);
+        body.call();
+    }
+    
+}
