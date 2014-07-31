5f5a28b2bf77c2f46d3784a2f40fd410650e039d
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2014-07-31 16:34:54 +0800

    [#8] Fix nullPointerException * contains object don't need to be create anymore. * Just the container object need to be initialized * Add Tests for play 1.2.7

--- samples-and-tests/just-test-cases/app/controllers/Application.java
+++ samples-and-tests/just-test-cases/app/controllers/Application.java
@@ -1,54 +1,43 @@
 package controllers;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TimeZone;
-
-import javax.persistence.Query;
-
 import models.Element;
 import models.Task;
-import play.Logger;
 import play.data.validation.Valid;
-import play.db.jpa.GenericModel.JPAQuery;
-import play.db.jpa.Model;
-import play.i18n.Messages;
 import play.mvc.Controller;
-import play.mvc.Util;
 
 public class Application extends Controller {
 
-    public static void index() {
-        Task task = new Task();
-        render(task); 
+  public static void index() {
+    Task task = new Task();
+    render(task);
+  }
+
+  public static void save(@Valid Task task) {
+    flash.clear();
+    if (validation.hasErrors()) {
+      flash.error("Data KO!!");
+    } else {
+      flash.success("Data OK!!");
     }
-    
-    public static void save(@Valid Task task) {
-	flash.clear();
-	if (validation.hasErrors()) {
-	    flash.error("Data KO!!");
-	} else {
-	    flash.success("Data OK!!");
-	}
-	render("Application/index.html", task);
+    render("Application/index.html", task);
+  }
+
+  public static void annotationJqValidate() {
+    Element element = new Element();
+    render(element);
+  }
+
+  public static void saveElement(@Valid Element element) {
+    if (element != null) {
+      validation.valid("element.task", element.task);
     }
-    
-    public static void annotationJqValidate() {
-	Element task = new Element();
-        render(task); 
+    if (validation.hasErrors()) {
+      validation.keep();
+      params.flash();
+      flash.error("Data KO!!");
+    } else {
+      flash.success("Data OK!!");
     }
-    
-    public static void saveElement(@Valid Element task) {
-	flash.clear();
-	if (validation.hasErrors()) {
-	    flash.error("Data KO!!");
-	} else {
-	    flash.success("Data OK!!");
-	}
-        render("Application/annotationJqValidate.html", task);
-    }
+    render("Application/annotationJqValidate.html", element);
+  }
 }
--- samples-and-tests/just-test-cases/app/models/Element.java
+++ samples-and-tests/just-test-cases/app/models/Element.java
@@ -1,38 +1,29 @@
 package models;
 
-import java.util.List;
-import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
-import play.*;
 import play.data.validation.Email;
-import play.data.validation.IPv4Address;
-import play.data.validation.IPv6Address;
 import play.data.validation.Match;
-import play.data.validation.Max;
-import play.data.validation.MaxSize;
-import play.data.validation.MinSize;
-import play.data.validation.Phone;
-import play.data.validation.Range;
 import play.data.validation.Required;
-import play.data.validation.URL;
-import play.data.validation.Valid;
 import play.db.jpa.Model;
-import data.validation.jqvalidate.HexColor;
 import data.validation.jqvalidate.JqValidate;
-
-import javax.persistence.*;
 
 @Entity
 public class Element extends Model {
-    
-    @JqValidate(forbiddenRules ={Required.class, Match.class, Email.class})
-    @Required
-    @Match("[A-Z]{3}")
-    @Email
-    public String noValidation;
-    
-    @JqValidate(allowedRules = {Required.class}, forbiddenRules ={ Match.class})
-    @Required
-    @Match("[A-Z]{3}")
-    public String requiredValidation;
+
+  @JqValidate(forbiddenRules = { Required.class, Match.class, Email.class })
+  @Required
+  @Match("[A-Z]{3}")
+  @Email
+  public String noValidation;
+
+  @JqValidate(allowedRules = { Required.class }, forbiddenRules = { Match.class })
+  @Required
+  @Match("[A-Z]{3}")
+  public String requiredValidation;
+
+  @ManyToOne
+  public Task task;
+
 }
