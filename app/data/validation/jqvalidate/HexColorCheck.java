730b61f9d009ee635caafa61dd7494e6ceaaae88
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2017-01-25 11:45:35 +0800

    * [#2] feat(validation): add Creditcard annotation ** will test luhn algorithm ** and test format: accept only spaces, digits and dashes

--- app/data/validation/jqvalidate/HexColorCheck.java
+++ app/data/validation/jqvalidate/HexColorCheck.java
@@ -11,30 +11,30 @@ import net.sf.oval.exception.OValException;
 /**
  * Validate HEX color code.
  *
- * Valid HEX code is :
- * Prefixed with a # character and composed of 6 or 3 characters ([A-F]|[0-9]) 
- * Regex code used : ^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$
+ * Valid HEX code is : Prefixed with a # character and composed of 6 or 3 characters ([A-F]|[0-9]) Regex code used :
+ * ^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$
+ * 
  * @author Nicolas Forney [nicolas@eforney.com]
  */
 public class HexColorCheck extends AbstractAnnotationCheck<HexColor> {
 
-    final static String mes = "validation.hexcolor";
-    private static final String HEX_PATTERN = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
-    private static final Pattern pattern = Pattern.compile(HEX_PATTERN);
+  final static String mes = "validation.hexcolor";
+  private static final String HEX_PATTERN = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
+  private static final Pattern pattern = Pattern.compile(HEX_PATTERN);
 
-    @Override
-    public void configure(HexColor hexColor) {
-	setMessage(hexColor.message());
-    }
+  @Override
+  public void configure(HexColor hexColor) {
+    setMessage(hexColor.message());
+  }
 
-    @Override
-    public boolean isSatisfied(Object validatedObject, Object value,
-	    OValContext context, Validator validator) throws OValException {
-	if (value == null || value.toString().length() == 0) {
-	    return true;
-	}
-	Matcher matcher = pattern.matcher(value.toString());
-	return matcher.matches();
+  @Override
+  public boolean isSatisfied(Object validatedObject, Object value, OValContext context, Validator validator)
+      throws OValException {
+    if (value == null || value.toString().length() == 0) {
+      return true;
     }
+    Matcher matcher = pattern.matcher(value.toString());
+    return matcher.matches();
+  }
 
 }
--- samples-and-tests/just-test-cases/app/models/Task.java
+++ samples-and-tests/just-test-cases/app/models/Task.java
@@ -1,57 +1,60 @@
 package models;
 
-import java.util.List;
-import java.util.Set;
+import javax.persistence.Entity;
 
-import play.*;
+import data.validation.jqvalidate.CreditCardNumber;
+import data.validation.jqvalidate.HexColor;
+import data.validation.jqvalidate.JqValidate;
 import play.data.validation.Email;
 import play.data.validation.IPv4Address;
 import play.data.validation.IPv6Address;
 import play.data.validation.Match;
-import play.data.validation.Max;
-import play.data.validation.MaxSize;
-import play.data.validation.MinSize;
 import play.data.validation.Phone;
 import play.data.validation.Range;
 import play.data.validation.Required;
 import play.data.validation.URL;
-import play.data.validation.Valid;
 import play.db.jpa.Model;
-import data.validation.jqvalidate.HexColor;
-import data.validation.jqvalidate.JqValidate;
-
-import javax.persistence.*;
 
 @Entity
 public class Task extends Model {
-    @Required public String details;
-    @Required @Range(max=10,min=1) public int priority;
-    @Required @Email public String authorEmail;
-    @Required @URL public String authorUrl;
-    
-    @IPv4Address public String authorIPv4;
-    @IPv6Address public String authorIPv6;
-    
-    @Phone public String authorPhone;
-    
-    @Match("[A-Z]{3}")
-    public String countryAbbreviation;
-    
-    @Match("^[0-9]{10,19}$")
-    public String creditCardNumber;
-    
-    @HexColor
-    public String color;
-    
-    @JqValidate(forbiddenRules ={Required.class, Match.class, Email.class})
-    @Required
-    @Match("[A-Z]{3}")
-    @Email
-    public String noValidation;
-    
-    @JqValidate(allowedRules = {Required.class}, forbiddenRules ={ Match.class, Email.class})
-    @Required
-    @Match("[A-Z]{3}")
-    @Email
-    public String requiredValidation;
+  @Required
+  public String details;
+  @Required
+  @Range(max = 10, min = 1)
+  public int priority;
+  @Required
+  @Email
+  public String authorEmail;
+  @Required
+  @URL
+  public String authorUrl;
+
+  @IPv4Address
+  public String authorIPv4;
+  @IPv6Address
+  public String authorIPv6;
+
+  @Phone
+  public String authorPhone;
+
+  @Match("[A-Z]{3}")
+  public String countryAbbreviation;
+
+  @CreditCardNumber
+  public String creditCardNumber;
+
+  @HexColor
+  public String color;
+
+  @JqValidate(forbiddenRules = { Required.class, Match.class, Email.class })
+  @Required
+  @Match("[A-Z]{3}")
+  @Email
+  public String noValidation;
+
+  @JqValidate(allowedRules = { Required.class }, forbiddenRules = { Match.class, Email.class })
+  @Required
+  @Match("[A-Z]{3}")
+  @Email
+  public String requiredValidation;
 }
